3fcb7551c7277aaae70b8d50189639e8970dd87e
Abhishek Singh <abhishek@Abhisheks-MacBook-Air.local>
2024-10-02 17:15:47 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Game from './Game';
+
+function App() {
+  
+  return (
+  <div className="App">
+    <h1>Super Tic Tac Toe</h1>
+    <Game />
+  </div>
+  );
+}
+
+
+export default App;
